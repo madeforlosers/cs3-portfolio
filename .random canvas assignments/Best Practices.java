a6e2b8163243b5215ac480af6c7ea03707ea4961
madeforlosers <87035377+madeforlosers@users.noreply.github.com>
2024-11-04 14:01:35 -0600

    Create Best Practices.java

---
+++ .random canvas assignments/Best Practices.java
@@ -0,0 +1,34 @@
+
+/**
+
+Program: Best Practices Example
+
+Author: Angel C.
+
+Description: The program declares a variable, initializes the variable, and then prints the value to the screen.
+
+(this is not my code -angel)
+
+**/
+
+public class Main {
+
+  public static void main (String args []) {
+
+      
+
+    /* Here is a multi-line comment
+
+    Notice how it extends over several lines. */
+
+    // declare a variable to represents someone's age
+
+    int age;
+
+    age = 16;   // assign the value 25 to the variable age
+
+    System.out.println("You are " + age + " years old.");
+
+  }
+
+}
