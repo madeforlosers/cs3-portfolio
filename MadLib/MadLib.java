7f50d5fd63870a252b5ec55b11c03d32d64d3e48
madeforlosers <87035377+madeforlosers@users.noreply.github.com>
2024-11-04 13:54:51 -0600

    Create MadLib.java

---
+++ MadLib/MadLib.java
@@ -0,0 +1,39 @@
+package Madlib;
+import java.util.Scanner;
+public class Madlib {
+
+	public static void main(String[] args) {
+		// SHUT UP I RUSHED THIS AND IT WASN'T INTENDED TO BE SEEN
+		// I swear I code better than this
+		// -angel
+	     Scanner input = new Scanner(System.in);
+	     System.out.println("Enter a first name: ");
+	     String firstName = input.nextLine();
+	     System.out.println("Enter a number: ");
+	     String childAge = input.nextLine();
+	     System.out.println("Enter a noun: ");
+	     String child = input.nextLine();
+	     System.out.println("Enter a noun: ");
+	     String typeToy = input.nextLine();
+	     System.out.println("Enter an adjective: ");
+	     String colorToy = input.nextLine();
+	     System.out.println("Enter an adjective: ");
+	     String typechild = input.nextLine();
+	     System.out.println("Enter an adjective: ");
+	     String np = input.nextLine();
+	     System.out.println("Enter an adjective: ");
+	     String sunday = input.nextLine();
+	     System.out.println("Enter a verb ending with -ing: ");
+	     String what = input.nextLine();
+	     System.out.println("Enter a verb ending with -ed: ");
+	     String eng = input.nextLine();
+	     System.out.println("Enter a verb: ");
+	     String share = input.nextLine();
+	     System.out.println("Enter an everyday object: ");
+	     String object = input.nextLine();
+	     System.out.println("\"I am " + childAge + " years old, so that means I get to have the " + colorToy + " " + typeToy + "!\" exclaimed the " + typechild + " " + child+".");
+	     System.out.println("\""+share+" with your sibling,\" " + firstName + " "+eng+", "+what+"ing over their "+np+", "+sunday+" "+object+".");
+	     input.close();
+	}
+
+}
